08933af2098e709d84909df0e392ac00d7754596
Shunichi Yamamoto <info@tkrworks.net>
2018-04-17 09:09:41 +0900

    Update setting and project files.

--- settings/uMi9s.Debug.cspy.bat
+++ settings/uMi9s.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.general.xcl" --backend -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0_2\common\bin\cspybat" -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.general.xcl" --backend -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0_2\common\bin\cspybat" -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.driver.xcl" 
 
 @echo off 
 :end
--- settings/uMi9s.Debug.cspy.ps1
+++ settings/uMi9s.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.general.xcl" --backend -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0_2\common\bin\cspybat" -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.general.xcl" --backend -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0_2\common\bin\cspybat" -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\shun\SimplicityStudio\v4_workspace\uMi9s\settings\uMi9s.Debug.driver.xcl" 
 }
